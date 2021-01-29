635b4f7e5b1b493d367d54d2c71aec13e8877523
Aaron Smith <kuroroRBM@gmail.com>
2021-01-28 20:22:13 -0500

    Fixed camera layer issues and added InputSys Package

--- Assets/Scripts/GameManager.cs
+++ Assets/Scripts/GameManager.cs
@@ -7,13 +7,23 @@ public class GameManager : Singleton<GameManager>
 {
     [SerializeField] int maxPlayers;
     [Range(1, 2)] public int playerNumber = 1;
-
+    [SerializeField] int startingLayerNumber = 14;
+    [SerializeField] LayerMask[] playerCamCullingMasks = { };
     [SerializeField] GameObject[] prefabs = { };
     
+
     // Start is called before the first frame update
     void Start()
     {
-        Instantiate(prefabs[Mathf.Clamp(playerNumber, 0, prefabs.Length - 1)]);
+        var prefabToSpawn = prefabs[Mathf.Clamp(playerNumber, 0, prefabs.Length - 1)];
+        var cameraMask = playerCamCullingMasks[Mathf.Clamp(playerNumber, 0, playerCamCullingMasks.Length - 1)];
+        
+        var playerGameObject = Instantiate(prefabToSpawn);
+        foreach(MeshRenderer mesh in playerGameObject.GetComponentsInChildren<MeshRenderer>()) 
+        {
+            mesh.gameObject.layer = startingLayerNumber + playerNumber;
+        }
+        playerGameObject.GetComponentInChildren<Camera>().cullingMask = cameraMask;
     }
 
 }
